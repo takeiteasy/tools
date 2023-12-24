da56ec911c56d1deedab47430138554114a37ccc
George Watson <gigolo@hotmail.co.uk>
2023-12-24 03:02:21 +0000

    Update wanko.rb

--- wanko.rb
+++ wanko.rb
@@ -18,78 +18,78 @@ COPYING, DISTRIBUTION AND MODIFICATION
 0. You just DO WHAT THE FUCK YOU WANT TO.
 =end
 
-begin
-  require 'nokogiri'
-  require 'slop'
-rescue
-  abort "#{$0} requires slop and nokogiri - Please run 'gem install nokogiri slop'"
-end
+require 'optparse'
+require 'nokogiri'
 
-$opts = Slop.parse do |o|
-  o.banner = " Usage: #{$0} [flags] [selector] [-f a:b:c] [-a href:title:src]"
-  o.bool  '-h', '--help', 'Prints help'
-  o.array '-f', '--files', 'Pass list of HTML/XML files', as: Array, delimiter: ':'
-  o.bool  '-x', '--xpath', 'Specify XPath selector'
-  o.array '-a', '--attr', 'Specify an attribute to print', as: Array, delimiter: ':'
-  o.bool  '-t', '--text', 'Output contents of elements'
-  o.bool  '-l', '--length', 'Output number of elements'
-end
+$attrs = []
+$files = []
+$xpath = false
+$printElements = false
+$printLength = false
+OptionParser.new do |o|
+    o.banner = " Usage: #{$0} [flags] [selector] [-f a:b:c] [-a href:title:src]"
+    o.on '-h', '--help', 'Print help' do
+        puts o
+        exit
+    end
+    o.on '-f', '--files A,B,C', Array, 'Pass list of HTML/XML files' do |a|
+        $files += a
+    end
+    o.on '-a', '--attrs A,B,C', Array, 'Specify an attributes to print' do |a|
+        $attrs += a
+    end
+    o.on '-x', '--xpath', 'Specify XPath selector' do
+        $xpath = true
+    end
+    o.on '-t', '--text', 'Output content of elements' do
+        $printElements = true
+    end
+    o.on '-l', '--length', 'Output number of elements' do
+        $printLength = true
+    end
+end.parse!
 
-if $opts.help? 
-  puts "\n#{$opts}\n"
-  exit 0
-end
-
-unless $opts.arguments.length == 1
-  puts "\n#{$opts}\n"
+unless ARGV.length == 1
   puts "ERROR: No CSS selector or XPath supplied\n"
   exit 1
 end
-$selector = $opts.arguments[0].split(',').map(&:strip)
 
-$type = $opts.xpath? ? :xpath : :css
+$selector = ARGV[0].split(',').map(&:strip)
+$type = $xpath ? :xpath : :css
 
 def doit x
-  length = 0
-  $selector.each do |s|
-    y = Nokogiri::HTML.parse(x).send($type, s)
-    length += y.length
-    if $opts.length?
-     next 
+    length = 0
+    $selector.each do |s|
+        y = Nokogiri::HTML.parse(x).send($type, s)
+        length += y.length
+        next if $printLength
+        y.each do |z|
+            if not $attrs.empty?
+                $opts[:attr].each do |a|
+                    b = z.attribute(a)
+                    unless b.to_s.to_s.strip.empty?
+                        puts b
+                    end
+                end
+            elsif $printElements
+                puts z.content unless z.content.empty?
+            else
+                o = z.to_s.split("\n").map(&:strip)
+                puts o.join() unless o.empty?
+            end
+        end
     end
-    y.each do |z|
-      if $opts.attr?
-        $opts[:attr].each do |a|
-          b = z.attribute(a)
-          unless b.to_s.to_s.strip.empty?
-            puts b
-          end
-        end
-      elsif $opts.text?
-        unless z.content.empty?
-          puts z.content
-        end
-      else
-        o = z.to_s.split("\n").map(&:strip)
-        unless o.empty?
-          puts o.join()
-        end
-      end
-    end
-  end
-  return length
+    return length
 end
 
 length = 0
-if $opts.files?
-  $opts[:files].each do |x|
-    length = doit File.read(x)
-  end
+if not $files.empty?
+    $files.each do |x|
+        length = doit File.read(x)
+    end
 else
-  length = doit STDIN.readlines.join()
+    length = doit STDIN.readlines.join()
 end
 
-if $opts.length?
-  puts length
-end
+puts length if $printLength
 
